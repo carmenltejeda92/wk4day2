dede7d98ec56fda69d4269c83f65b07fb23cdc4b
carmenltejeda92 <carmenltejeda92@gmail.com>
2023-10-26 19:20:10 -0400

    Final commit for Review Lab

--- reviewLab.js
+++ reviewLab.js
@@ -32,19 +32,19 @@
 /* B. Strings */
 
 // 1. Create a variable called firstVariable
-let firstVariable;
+// let firstVariable;
 
 // 2. Assign it the value of the string "Hello World"
-firstVariable = 'Hello World'
+// firstVariable = 'Hello World'
 
 // 3. Change the value of this variable to some number
-firstVariable = 2
+// firstVariable = 2
  
 // 4. Store the value of firstVariablein a new variable called secondVariable
-let secondVariable = firstVariable
+// let secondVariable = firstVariable
 
 // 5. Change the value of secondVariablevto any string.
-secondVariable = 'any string'
+// secondVariable = 'any string'
 
 //6. What is the value of firstVariable?
 // console.log(firstVariable)
@@ -53,8 +53,8 @@ secondVariable = 'any string'
 // 7. Create a variable called yourName and set it equal to your name as a string. 
 //    Then, write an expression that takes the string "Hello, my name is " and the variable yourNameso that it returns a new string with them concatenated.
 //      ex: Hello, my name is Jean Valjean
-let yourName = 'Carmen'
-let greetings = ('Hello, my name is ' + yourName)
+// let yourName = 'Carmen'
+// let greetings = ('Hello, my name is ' + yourName)
 // console.log(greetings)
 
 
@@ -243,7 +243,7 @@ Note: You don't really need .splice()for these. You could use it, but there are 
 
 Given the following array: const myArray = [5, 10, 500, 20] */
 
-const myArray = [5, 10, 500, 20]
+// const myArray = [5, 10, 500, 20]
 
 //1. Add the string "Aegon"to the end of the array. Add another string of your choice to the end of the array.
 // myArray.push("Carmen")
@@ -387,7 +387,7 @@ Write a function printCoolthat accepts one parameter, nameas an argument. The fu
 /* C. calculateCube
 Write a function calculateCubethat takes a single number and prints the volume of a cube made from that number. */
 // function calculateCubethat(a){
-//     return a*a*a
+//     return a**3
 // }
 
 // console.log(calculateCubethat(5))
@@ -427,79 +427,250 @@ each number is the length of the corresponding string. */
 Write a function getMultipleLengthsthat accepts a single parameter as an argument: an array of strings. 
 The function should return an array of numbers where each number is the length of the corresponding string. */
 
-function getMultipleLengthsthat(a){
-    b = a.length
-    console.log(b)
-}
+// function getMultipleLengthsthat(a){
+//     const b = []
+//     for(let i=0; i<a.length; i++){
+//         b[i] = a[i].length
+//     }
+//     console.log(b)
+// }
 
-getMultipleLengthsthat(['string', 'two', 'seven'])
+// getMultipleLengthsthat(['twelve', 'two', 'seven'])
+
 
+/* G. maxOfThree
+Define a function maxOfThreethat takes three numbers as arguments and returns the largest of them. If all numbers 
+are the same, it doesn't matter which one is returned. If the two largest numbers are the same, one of them should 
+be returned. Be sure to test it with larger values in each of the three locations. */
 
+// function maxOfThree(a, b, c){
+//     if (a==b || a ==c || b==c) console.log(a + ' is the first number on the list')
+//     else if (b >c && b > a) console.log(b + ' is the largest number')
+//     else if (c > a && c > b) console.log(c + ' is the largest number')
+//     else if(a > b && a > c) console.log(a + " is the largest number")
+// }
 
+// maxOfThree(5, 3 ,3)
 
 
+/* H. printLongestWord
+Write a function printLongestWord that accepts a single argument, an array of strings. The method should return the 
+longest word in the array. In case of a tie, the method should return the word that appears first in the array. */
 
+// function printLongestWord(a){
 
+//     b = a[0].length
+//     c = a[0]
+//     for(let i=0; i<a.length; i++){
+//         d = a[i].length
+//         if(d !== b){
+//             c = a[i]
+//             b = d
+//         } else if(d === b){
+//             console.log(c)
+//         }
+//     }
+// }
 
+// printLongestWord(['bird', 'farm', 'elephant'])
 
 
 
+
+/* Objects */
+/*A. Make a user object
+1. Create an object called user.
+2. Write in to the object the key-value pairs for name, email, age, and purchased. Set the value of purchased,to an empty array []. 
+    Set the other values to whatever you would like. */
 
+// const user = {
+//     name: 'Carmen',
+//     email: 'carmen@email.com',
+//     age: 31,
+//     purchased: []
+// }
+// console.log(user)
 
+/* B. Update the user
+1. Our user has changed his or her email address. Without changing the original userobject, update the email value to a new email address.
+2. Our user has had a birthday! Without changing the original userobject, increment the agevalue using the postfix operator. Hint: age++ */
 
+// user.email = 'carment91@email.com'
+// console.log(user)
+// user.age++
+// console.log(user)
 
 
+/* C. Adding keys and values
+You have decided to add your user's location to the data that you want to collect.
+1. Without changing the original userobject, add a new key location to the object, and give it a value or some-or-other location (a string). */
+// user.location = 'Youngstown'
+// console.log(user)
 
+/* D. Shopaholic! */
+//1. Our user has purchased an item! They have purchased some "carbohydrates". Using .push(), add the string "carbohydrates" to the purchasedarray.
+// user.purchased.push('carbohydrates')
+// console.log(user)
 
+//2. Our user has purchased an item! They have purchased some "peace of mind". Using .push(), add the string "peace of mind" to the purchasedarray.
+// user.purchased.push('peace of  mind')
+// console.log(user)
 
+//3. Our user has purchased an item! They have purchased some "Merino jodhpurs". Using .push(), add the string "Merino jodhpurs" to the purchasedarray.
+// user.purchased.push('Merino jodhpurs')
+// console.log(user)
 
+//4. Console.log just the "Merino jodhpurs" from the purchasedarray.
+// console.log(user.purchased[2])
 
 
+/* E. Object-within-object */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//1. Write a friendobject into your userobject and give the friend a name, age, location, and purchased array (empty for now)
+// user.friend = {
+    // name: 'GG',
+    // age: 31,
+    // location: 'Wisconsin',
+    // purchased: []
+// }
+
+// 2. Console.log just the friend's name
+// console.log(user.friend.name)
+
+//3. Console.log just the friend's location
+// console.log(user.friend.location)
+
+//4. CHANGE the friend's age to 55
+// user.friend.age = 55
+// console.log(user.friend.age)
+
+//5. The friendhas purchased "The One Ring". Use .push()to add "The One Ring" to the friend's purchasedarray.
+// user.friend.purchased.push('The One Ring')
+// console.log(user.friend.purchased)
+
+//6. The friendhas purchased "A latte". Use .push()to add "A latte" to the friend's purchasedarray.
+// user.friend.purchased.push('A latte')
+// console.log(user.friend.purchased)
+
+//7. Console.log just "A latte" from the friend's purchasedarray.
+// console.log(user.friend.purchased[1])
+
+
+
+/* F. Loops */
+//1. Write a for loop that iterates over the User's purchasedarray (NOT the friend's purchased array), and prints each element to the console.
+// function getPurchased(a){
+//     const b = []
+//     for(let i=0; i<a.length; i++){
+//         b[i] = a[i]
+//     }
+//     console.log(b)
+// }
+
+// getPurchased(user.purchased)
+
+//2. Write a for loop that iterates over the Friend's purchasedarray, and prints each element to the console.
+// function getPurchased(a){
+//     const b = []
+//     for(let i=0; i<a.length; i++){
+//         b[i] = a[i]
+//     }
+//     console.log(b)
+// }
+
+// getPurchased(user.friend.purchased)
+
+
+/* G. Functions can operate on objects
+1. Write a single function updateUser that takes no parameters. When the function is run, it should: 
+    1. it should increment the user's age by 1
+    2. make the user's name uppercase */
+
+// function updateUser(){
+//     user.age++
+//     changedName = user.name.charAt(0).toUpperCase() + user.name.slice(1)
+//     console.log(changedName)
+// }
+
+// updateUser()
+
+// 2. Write a function oldAndLoud that performs the exact same tasks as updateUser, but instead of hard-coding 
+// it to only work on our userobject, make it take a parameter person, and have it modify the object that is 
+// passed in as an argument when the function is called. Call your oldAndLoudfunction with user as the argument.
+
+// function oldAndLoud(person){
+//     user.age++
+//     user.name = user.name.charAt(0).toUpperCase() + user.name.slice(1)
+//     console.log(user)    
+// }
+
+// oldAndLoud(user)
+
+
+/* Cat Combinator */
+
+/* 1. Mama cat
+Define an object called cat1that contains the following properties:
+    -name
+    -breed
+    -age (a number)
+console.log the cat's age
+console.log the cat's breed */
+
+// const cat1 = {
+//     name: 'thomas',
+//     breed: 'siamese',
+//     age: 3
+// }
+// console.log(cat1.age)
+// console.log(cat1.breed)
+
+/* 2. Papa cat
+Define an object called cat2that also contains the properties:
+    name
+    breed
+    age (a number) */
+
+    // const cat2 = {
+    //     name: 'shadow',
+    //     breed: 'siamese',
+    //     age: 2
+    // }
+
+/* 3. Combine Cats!
+Write a function combineCatsthat has two parameters mama, and papa. The function will take two arguments -- each a cat object.
+Pass cat1and cat2as arguments to the combineCatsfunction. The function should console.log them. */
+
+// function combineCats(mama, papa){
+//     console.log(mama, papa)
+// }
+
+// combineCats(cat1, cat2)
+
+
+/* Make it so the combineCatsfunction will return a combination of the two incoming cats
+    The result should be an object wherein the
+        -name is a concatenation of the parents' names
+        -the age is 1
+        -the breed is each of the parents' breeds with a hyphen in between */
+
+        
+// function combineCats(mama, papa){
+//     const both = {
+//         name: mama.name + papa.name,
+//         age: 1,
+//         breed: mama.breed +'-'+ papa.breed
+//     }
+//     console.log(both)
+// }
+
+// combineCats(cat1, cat2)
+
+
+/* 4. Cat brain bender
+Write a console.log that is three levels deep of combineCats. combineCats should 
+have two arguments, each which are combineCats, each which have two arguments, each which are combineCats. */
+
+// console.log(combineCats(combineCats(combineCats(cat1, cat2), combineCats(cat1, cat2)), combineCats(combineCats(cat1, cat2), combineCats(cat1, cat2))));
 
 
